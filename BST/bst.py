2bf7052c7b02bc454424f09d54ac18c4b1205fd4
Shivansh Gupta <shivanshgupta2057@gmail.com>
2024-05-16 11:55:23 +0530

    bst updated

--- BST/bst.py
+++ BST/bst.py
@@ -30,6 +30,15 @@ class BinarySearchTree:
                     return True
                 temp = temp.right
     
+    def r_insert(self, root, value):
+        if root == None:
+            return Node(value)
+        if value < root.value:
+            root.left = self.r_insert(root.left, value)
+        if value > root.value:
+            root.right = self.r_insert(root.right, value)
+        return root
+    
     def contains(self, value):
         temp = self.root
         while temp is not None:
@@ -40,3 +49,56 @@ class BinarySearchTree:
             else:
                 return True
         return False
+    
+    def r_contains(self, root, value):
+        if root == None:
+            return False
+        if root.value == value:
+            return True
+        if value < root.value:
+            return self.r_contains(root.left, value)
+        if value > root.value:
+            return self.r_contains(root.right, value)
+    
+    def r_delete(self, root, value):
+        if root == None:
+            return None
+        if value < root.value:
+            root.left = self.r_delete(root.left, value)
+        elif value > root.value:
+            root.right = self.r_delete(root.right, value)
+        else:
+            if root.left == None and root.right == None:
+                return None
+            elif root.left == None:
+                return root.right
+            elif root.right == None:
+                return root.left
+            else:
+                sub_tree_min = self.min_value(root.right)
+                root.value = sub_tree_min
+                root.right = self.r_delete(root.right, sub_tree_min)
+        return root
+    
+    def min_value(self, root):
+        while root.left is not None:
+            root = root.left
+        return root.value
+
+
+my_bst = BinarySearchTree()
+root = my_bst.r_insert(my_bst.root, 47)
+my_bst.r_insert(root, 21)
+my_bst.r_insert(root, 97)
+my_bst.r_insert(root, 14)
+my_bst.r_insert(root, 24)
+my_bst.r_insert(root, 84)
+my_bst.r_insert(root, 22)
+my_bst.r_insert(root, 26)
+
+
+print(my_bst.r_contains(root, 21))
+
+my_bst.r_delete(root, 21)
+
+print(my_bst.r_contains(root, 21))
